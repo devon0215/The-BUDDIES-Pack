cad51f2631ddbb5031e52426ffaf97b0bf785921
devon0215 <31562080+devon0215@users.noreply.github.com>
2022-09-10 03:40:34 -0400

    Initial upload
    Adding the files I have as of now for the BUDDIES main mod.

---
+++ Files/PBO Files/Mod.cpp
@@ -0,0 +1,7 @@
+name="The B.U.D.D.I.E.S. Pack";
+picture="logo_buddies_2048_1024.paa";
+logoSmall="logo_buddies_64_64.paa";
+logo="logo_buddies_128_128.paa";
+action="https://steamcommunity.com/sharedfiles/filedetails/?id=1466071662";
+tooltipOwned="The B.U.D.D.I.E.S. Pack";
+overview="This is the official mod of the B.U.D.D.I.E.S. arma group. This mod is still very much W.I.P., so if you find any issues with it, or have an idea for what should be in the mod, please say so on the workshop page.";
